4dd2a4d2166730b66d5a57b7c18a201aebb84adb
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2024-12-19 10:40:49 -0300

    test nginx

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,17 +6,12 @@ test("has title", async ({ page }) => {
   await page.goto(url);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/nginx/);
 });
 
 test("get started link", async ({ page }) => {
   await page.goto(url);
 
-  // Click the get started link.
-  await page.getByRole("link", { name: "Get started" }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(
-    page.getByRole("heading", { name: "Installation" })
-  ).toBeVisible();
+  // Check for welcome text
+  await expect(page.getByText("Welcome to nginx!")).toBeVisible();
 });
