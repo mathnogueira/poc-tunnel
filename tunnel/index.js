73e4412df5568bbe4a17ae32329afbcef947e2e2
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2024-12-19 17:08:58 -0300

    use local tunnel instead

--- tunnel/index.js
+++ tunnel/index.js
@@ -1,15 +1,12 @@
-const ngrok = require("@ngrok/ngrok");
+const localtunnel = require("localtunnel");
 const { spawn } = require("child_process");
 
 (async function () {
-  const listener = await ngrok.forward({
-    addr: 80,
-    authtoken_from_env: true,
-  });
+  const tunnel = await localtunnel({ port: 80 });
 
   console.log(`Ingress established at: ${listener.url()}`);
 
-  process.env.TARGET_URL = listener.url();
+  process.env.TARGET_URL = tunnel.url;
 
   const playwright = spawn("npx", ["playwright", "test"]);
 
