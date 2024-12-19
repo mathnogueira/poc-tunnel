d62f9aa36d03700c769cefc0c4183dfa3fb30054
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2024-12-19 10:38:05 -0300

    call playwright from node

--- tunnel/index.js
+++ tunnel/index.js
@@ -1,4 +1,5 @@
 const ngrok = require("@ngrok/ngrok");
+const { spawn } = require("child_process");
 
 (async function () {
   const listener = await ngrok.forward({
@@ -7,4 +8,21 @@ const ngrok = require("@ngrok/ngrok");
   });
 
   console.log(`Ingress established at: ${listener.url()}`);
+
+  process.env.TARGET_URL = listener.url();
+
+  const playwright = spawn("npx", ["playwright", "test"]);
+
+  playwright.stdout.on("data", (data) => {
+    console.log(`Playwright output: ${data}`);
+  });
+
+  playwright.stderr.on("data", (data) => {
+    console.error(`Playwright error: ${data}`);
+  });
+
+  playwright.on("close", (code) => {
+    console.log(`Playwright process exited with code ${code}`);
+    process.exit(code);
+  });
 })();
